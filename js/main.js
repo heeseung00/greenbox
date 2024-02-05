8594d64ded02ff2d7ec348b279b3d46fa28e4b96
heeseung <love01040701320@gmail.com>
2024-02-05 13:08:54 +0900

    베스트까지 작업

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+    // 스크롤 할 때, nav 상단 고정
+    window.addEventListener('scroll', function() {
+        var nav = document.querySelector('.nav');
+        var header = document.querySelector('.header');
+        var inner = document.querySelector('.inner');
+        var headerHeight = header.offsetHeight;
+        
+        if (window.pageYOffset >= headerHeight) {
+            nav.style.position = 'fixed';
+            nav.style.top = '0';
+            inner.classList.add('scrolled'); // 클래스 추가
+        } else {
+            nav.style.position = 'relative';
+            nav.style.top = '0';
+            inner.classList.remove('scrolled'); // 클래스 제거
+        };
+    });
+});
